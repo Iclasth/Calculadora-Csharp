e1388edc3d05da39834fa58b869cd5d19970e7be
iclasTH <iclasinprogramacao@gmail.com>
2025-06-27 14:32:40 -0300

    Adicionando looping ao menu

--- Program.cs
+++ Program.cs
@@ -12,72 +12,86 @@ namespace UtilizandoOperadoresAritimeticos.CalculadoraAritimetica
         static void Main(string[] args)
         {
             Calculadora calculator = new Calculadora();
+            bool menu = true;
 
-            Console.WriteLine("Calculadora Aritmética");
-            Console.WriteLine("Escolha a operação: +, -, *, /, %, **, seno, cosseno, tangente");
-            string operacao = Console.ReadLine();
-
-            if (operacao == "seno" || operacao == "cosseno" || operacao == "tangente")
+            while (menu)
             {
-                Console.WriteLine("Digite o ângulo em graus:");
-                double angulo = Convert.ToDouble(Console.ReadLine());
-                switch (operacao)
+                Console.Clear();
+                Console.WriteLine("Calculadora Aritmética");
+                Console.WriteLine("Escolha a operação: +, -, *, /, %, **, seno, cosseno, tangente");
+                string operacao = Console.ReadLine();
+
+                if (operacao == "seno" || operacao == "cosseno" || operacao == "tangente")
                 {
-                    case "seno":
-                        calculator.Seno(angulo);
-                        break;
-                    case "cosseno":
-                        calculator.Coseno(angulo);
-                        break;
-                    case "tangente":
-                        calculator.Tangente(angulo);
-                        break;
-                    default:
-                        Console.WriteLine("Operação inválida para ângulos.");
-                        break;
-                }
-                return;
-            }
-            else if (operacao == "raiz quadrada")
-            {
-                Console.WriteLine("Digite o número para calcular a raiz quadrada:");
-                double numero = Convert.ToDouble(Console.ReadLine());
-                calculator.RaizQuadrada(numero);
-                return;
-            }
-            else if (operacao == "+" || operacao == "-" || operacao == "*" || operacao == "/" || operacao == "%" || operacao == "**")
-            {
-                Console.WriteLine("Digite o primeiro número:");
-                int n1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Digite o segundo número:");
-                int n2 = Convert.ToInt32(Console.ReadLine());
-
-                switch (operacao)
-                {
-                    case "+":
-                        calculator.Somar(n1, n2);
-                        break;
-                    case "-":
-                        calculator.Subtrair(n1, n2);
-                        break;
-                    case "*":
-                        calculator.Multiplicar(n1, n2);
-                        break;
-                    case "/":
-                        calculator.Dividir(n1, n2);
-                        break;
-                    case "%":
-                        calculator.Modular(n1, n2);
-                        break;
-                    case "**":
-                        calculator.Potencia(n1, n2);
-                        break;
-
-                    default:
-                        Console.WriteLine("Digite, por favor, uma operação válida.");
-                        break;
+                    Console.WriteLine("Digite o ângulo em graus:");
+                    double angulo = Convert.ToDouble(Console.ReadLine());
+                    switch (operacao)
+                    {
+                        case "seno":
+                            calculator.Seno(angulo);
+                            break;
+                        case "cosseno":
+                            calculator.Coseno(angulo);
+                            break;
+                        case "tangente":
+                            calculator.Tangente(angulo);
+                            break;
+                        default:
+                            Console.WriteLine("Operação inválida para ângulos.");
+                            break;
+                    }
 
                 }
+                else if (operacao == "raiz quadrada")
+                {
+                    Console.WriteLine("Digite o número para calcular a raiz quadrada:");
+                    double numero = Convert.ToDouble(Console.ReadLine());
+                    calculator.RaizQuadrada(numero);
+
+                }
+                else if (operacao == "+" || operacao == "-" || operacao == "*" || operacao == "/" || operacao == "%" || operacao == "**")
+                {
+                    Console.WriteLine("Digite o primeiro número:");
+                    int n1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Digite o segundo número:");
+                    int n2 = Convert.ToInt32(Console.ReadLine());
+
+                    switch (operacao)
+                    {
+                        case "+":
+                            calculator.Somar(n1, n2);
+                            break;
+                        case "-":
+                            calculator.Subtrair(n1, n2);
+                            break;
+                        case "*":
+                            calculator.Multiplicar(n1, n2);
+                            break;
+                        case "/":
+                            calculator.Dividir(n1, n2);
+                            break;
+                        case "%":
+                            calculator.Modular(n1, n2);
+                            break;
+                        case "**":
+                            calculator.Potencia(n1, n2);
+                            break;
+
+                        default:
+                            Console.WriteLine("Digite, por favor, uma operação válida.");
+                            break;
+
+                    }
+
+                    Console.WriteLine("Deseja realizar outra operação? (s/n)");
+                    string resposta = Console.ReadLine().ToLower();
+                    if (resposta != "s")
+                    {
+                        menu = false;
+                        Console.WriteLine("Obrigado por utilizar a calculadora!");
+                    }
+                }
+
             }
         }
     }
